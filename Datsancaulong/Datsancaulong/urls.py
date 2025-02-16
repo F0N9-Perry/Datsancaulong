9cff33b549f4fa2dda7f444a4edb98afe6a39bce
NguyenThiNhuYen <yenntn7598@ut.edu.vn>
2025-02-16 23:11:36 +0700

    update

--- Datsancaulong/Datsancaulong/urls.py
+++ Datsancaulong/Datsancaulong/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from adminsystem import views as adminsystem 
 
 urlpatterns = [
     path('', include('adminsystem.urls')),
     path('admin/', admin.site.urls),
+    path('',adminsystem.adminsystem)
 ]
