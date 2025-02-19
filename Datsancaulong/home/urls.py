e0ef71b23d44ff187dc1275fdc6813769cd451d6
NguyenThiNhuYen <yenntn7598@ut.edu.vn>
2025-02-19 17:21:37 +0700

    update home/urls

--- Datsancaulong/home/urls.py
+++ Datsancaulong/home/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('', view.home, name="home")
+    path('', views.home, name="home")
 
 ]
