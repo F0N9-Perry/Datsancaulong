cad2b8eec0bc57a2cac61cb14f6c1169718ab337
NguyenThiNhuYen <yenntn7598@ut.edu.vn>
2025-02-20 11:52:18 +0700

    updateupdate

--- Datsancaulong/home/urls.py
+++ Datsancaulong/home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('lichdatsan/', views.lichdatsan, name="lichdatsan"),
+    path('edit-lichdatsan/', views.edit_lichdatsan, name="edit-lichdatsan")
 
 ]
--- Datsancaulong/home/views.py
+++ Datsancaulong/home/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from adminsystem.models import LichDatSan
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, "home/home.html")
+    return render(request, "home.html")
+
+def lichdatsan(request):
+    lichdatsan = LichDatSan.objects.all()
+    template = loader.get_template('home/LichDatSan.html')
+    context = {
+        'lichdatsan': lichdatsan,
+    }
+    return HttpResponse(template.render(context, request))
+
+def edit_lichdatsan(request):
+    lichdatsan = LichDatSan.objects.get( id = 1)
+    template= loader.get_template('home/LichDatSan-edit.html')
+    context = {
+        'lichdatsan': lichdatsan,
+    }
+    return HttpResponse(template.render(context, request))
